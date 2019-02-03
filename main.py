ae8af2c45d409f4eb5d235db8c4306786773dfcc
Russell <russellcaletena@gmail.com>
2019-02-03 06:45:12 -0800

    Minor fixes to create functional code

--- main.py
+++ main.py
@@ -2,33 +2,50 @@
 import cv2
 import numpy as np
 
-#importing google text to speech api
+# Import Google Text-To-Speech API
 from gtts import gTTS
 import os
 
+print ('Generating MP3 files and setting up image configurations...')
+
+# Configuration of Text to Speech Specifications
+language = 'en'
+redString = 'The color is red'
+yellowString = 'The color is yellow'
+greenString = 'The color is green'
+
+# Creation of mp3 files for speech
+# Passing the language and test to the engine
+redObject = gTTS(text=redString, lang=language, slow=False)
+redObject.save("red.mp3")
+yellowObject = gTTS(text=yellowString, lang=language, slow=False)
+yellowObject.save("yellow.mp3")
+greenObject = gTTS(text=greenString, lang=language, slow=False)
+greenObject.save("green.mp3")
+
+# Create a color null color variable
+color = None
+
+# Specify text formatting for OpenCV window
+font = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (100,450)
+fontScale = 1
+fontColor = (255,255,255)
+lineType = 2
+
+'''
+# Google Initialization
+from google.cloud import texttospeech
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+
+# Set the text input to be synthesized
+synthesis_input = texttospeech.types.SynthesisInput(text="Hello, World!")
+'''
+
 # Start Video Capture
 cap = cv2.VideoCapture(0)
-# Language in which you want to convert
-language = 'en'
-#variables for speech
-str_red = 'the Color is Red'
-str_yellow = 'the Color is Yellow'
-str_green = 'the Color is Green'
-
-#creating mp3 files for speech
-#passing the text and lanuguage to the engine
-#slow=False to tell the module that the converted audio should have high speed
-redobj = gTTS(text=str_red, lang=language, slow=False)
-redobj.save("red.mp3")
-
-yellowobj = gTTS(text=str_yellow, lang=language, slow=False)
-yellowobj.save("yellow.mp3")
-
-greenobj = gTTS(text=str_green, lang=language, slow=False)
-greenobj.save("green.mp3")
-
-#color = None
-
 
 # Indefinitely initiate Video Stream
 while(True):
@@ -49,8 +66,11 @@ while(True):
                           'yellow' : (  0,255,217),
                           'green'  : (  0,255,  0)}
 
+    # Smoothen the image with a Gaussian blur
+    frameBlur = cv2.GaussianBlur(frame, (5,5),0)
+
     # Convert raw image data to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frameBlur, cv2.COLOR_BGR2HSV)
 
     # Apply colored-mask for each color
     red_image_mask = cv2.inRange(hsv, lowerBoundDict['red'], higherBoundDict['red'])
@@ -58,9 +78,9 @@ while(True):
     green_image_mask = cv2.inRange(hsv, lowerBoundDict['green'], higherBoundDict['green'])
 
     # Add bitwise functionality
-    red_res = cv2.bitwise_and(frame,frame, mask=red_image_mask)
-    yellow_res = cv2.bitwise_and(frame,frame, mask=yellow_image_mask)
-    green_res = cv2.bitwise_and(frame,frame, mask=green_image_mask)
+    red_res = cv2.bitwise_and(frameBlur,frameBlur, mask=red_image_mask)
+    yellow_res = cv2.bitwise_and(frameBlur,frameBlur, mask=yellow_image_mask)
+    green_res = cv2.bitwise_and(frameBlur,frameBlur, mask=green_image_mask)
 
     # Convert colored-images to grayscale
     redToGray = cv2.cvtColor(red_res, cv2.COLOR_BGR2GRAY)
@@ -68,16 +88,19 @@ while(True):
     greenToGray = cv2.cvtColor(green_res, cv2.COLOR_BGR2GRAY)
 
     # Resize the image to a smaller size
-    resizedFrame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+    #resizedFrame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
     resizedRed = cv2.resize(redToGray, (0,0), fx=0.5, fy=0.5)
     resizedYellow = cv2.resize(yellowToGray, (0,0), fx=0.5, fy=0.5)
     resizedGreen = cv2.resize(greenToGray, (0,0), fx=0.5, fy=0.5)
 
+    # Display current color on the screen
+    cv2.putText(frame, 'Current Color is: {}'.format(color), bottomLeftCornerOfText, font, fontScale, fontColor, lineType)
+
     # Display windows
-    cv2.imshow('frame',resizedFrame)
-    cv2.imshow('red', resizedRed)
-    cv2.imshow('yellow', resizedYellow)
-    cv2.imshow('green', resizedGreen)
+    cv2.imshow('Raw Frame',frame)
+    cv2.imshow('Red Mask', resizedRed)
+    cv2.imshow('Yellow Mask', resizedYellow)
+    cv2.imshow('Green Mask', resizedGreen)
 
     # Count the the total number of red, yellow, and green pixels
     redPixelCount = cv2.countNonZero(redToGray)
@@ -85,34 +108,38 @@ while(True):
     greenPixelCount = cv2.countNonZero(greenToGray)
 
     # Determine strongest color
-    # Say the color out loud
-
     if (redPixelCount > yellowPixelCount) and (redPixelCount > greenPixelCount):
-        #currentColor = red
-        #if currentColor != red:
+        currentColor = 'Red'
+        if currentColor != color:
+            color = 'Red'
+            print ('the Color is Red')
 
-        str_red = 'the Color is Red'
-        print (str_red)
-
-        #opens mp3 file (w/default) and plays audio
-        os.system("red.mp3")
+            # Opens generated mp3 file and plays audio
+            os.system('red.mp3')
 
     elif (yellowPixelCount > redPixelCount) and (yellowPixelCount > greenPixelCount):
-        #cplor = yellow
-        print (str_yellow)
+        currentColor = 'Yellow'
+        if currentColor != color:
+            color = 'Yellow'
+            print ('the Color is Yellow')
 
-        #opens mp3 file (w/default) and plays audio
-        os.system("yellow.mp3")
+            # Opens generated mp3 file and plays audio
+            os.system('yellow.mp3')
+
     elif (greenPixelCount > redPixelCount) and (greenPixelCount > yellowPixelCount):
-        #color = green
-        print (str_green)
+        currentColor = 'Green'
+        if currentColor != color:
+            color = 'Green'
+            print ('the Color is Green')
 
-        #opens mp3 file (w/default) and plays audio
-        os.system("green.mp3")
+            # Opens generated mp3 file and plays audio
+            os.system('green.mp3')
+            
     else:
-        print ('No Conclusion')
-
-
+        currentColor = None
+        if currentColor != color:
+            color = None
+            print ('the Color is None')
 
     # Stop the program
     if cv2.waitKey(1) & 0xFF == ord('q'):
